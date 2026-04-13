a92289bb1e977a11b361672edcb6330cac94fc04
badrul <mhdbadrul.masrol@myeg.com.my>
2026-04-13 11:09:14 +0800

    Test auto-deployment

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,7 +65,7 @@
                                 Read the
                                 <a href="https://laravel.com/docs" target="_blank" class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
                                     <span>Documentation</span>
-                                    <h2>🚀  Deployed via GitHub Actions at {{ now() }}</h2>
+                                    <h2>🚀 Deployed via GitHub Actions at {{ now() }}</h2>
 
                                     <svg
                                         width="10"
